bb224c90886e36e8b61df48424540b7469ecc8cd
Amy <amywaldrop@mail.weber.edu>
2025-02-06 10:01:08 -0700

    REF:(refactor)while loop for invalid input

--- game.py
+++ game.py
@@ -29,5 +29,17 @@ A faint path lies ahead, leading deeper into the unknown...
 print(starting_area)
 
 # Ask the player for their first decision
-decision = input("Do you wish to take the path? (yes or no): ")
-print(decision)
+decision = input("Do you wish to take the path? (yes or no): ").lower()
+
+
+# invalid response
+while decision not in ["yes", "no"]:
+    print('Confused, you stand still, unsure of what to do. Please type "yes" or "no"')
+    # option for user to make new decision
+    decision = input("Do you wish to take the path? (yes or no): ").lower() 
+# Respond based on the player's decision
+if decision == 'yes':
+    print(f'Brave choice, {player_name}! You step on the path and venture forward')
+elif decision == 'no':
+    print(f'{player_name}, you decide to wait. Perhaps courage will find you later.')
+# 
